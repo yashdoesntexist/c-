c18e10fcf33847402e64c2c7ca33eb86377e76f9
agentP <learnhacksandtricks@gmail.com>
2025-10-28 23:38:50 -0700

    c#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const float pi = 3.14F;
+
+            Console.Write("Enter the radius of the circle: ");
+
+            int radius = int.Parse(Console.ReadLine());
+
+            float ans = pi * radius * radius;
+
+            Console.WriteLine("Area of your circle is " + ans);
+        }
+    }
+}
